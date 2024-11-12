c471a3e42bc98fccccf32a0b202e9f55e9a7d529
monickaa <147165950+monickaa@users.noreply.github.com>
2024-11-12 16:35:57 +0530

    Create commandLineArgument.java

---
+++ commandLineArgument.java
@@ -0,0 +1,8 @@
+public class Main{
+    public static void main(String[] args) {
+        int n =args[0].length();
+        for(int i=0;i<n;i++){
+             System.out.println(args[0]);
+        }
+    }
+}
